27039c73bdd11147b834242b4ef3a5d8ff5d9ba4
peppelin <peppelin@gmail.com>
2022-09-28 12:28:00 +0200

    Adding view and create routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,12 +6,20 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello world!"))
+	w.Write([]byte("hello snippetbox!"))
+}
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Here you can view some snippets"))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Here you can create some snippets"))
 }
 func main() {
 	// We initialize a new ServerMux and assign the home to the main handler
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use listen and serve to start the new server.
 
