5128b96e1c094f5c2addd8a7b5ed862af2f7269b
peppelin <peppelin@gmail.com>
2022-09-29 13:45:00 +0200

    adding static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,15 @@ import (
 func main() {
 	// We initialize a new ServerMux and assign the handlers
 	mux := http.NewServeMux()
+
+	// Serving static files
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
